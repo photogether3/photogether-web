f8675af1d9dcf7cd4f1765201193d4270b970afa
dev.goraebap <dev.goraebap@gmail.com>
2025-03-01 00:01:05 +0900

    feat: add email check usecase

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::UserController < Api::ApplicationApiController
+  before_action :authenticate_user!, except: [ :is_email_taken, :update_password_by_otp ]
+  before_action :ensure_valid_email, only: [ :is_email_taken ]
+
   def is_email_taken
-    puts "User is email taken"
+    user = User.find_by(email_address: params[:email])
+    render json: { is_duplicated: !user.nil? }, status: :ok
   end
 
   def show
     puts "User show"
+    puts @current_user.inspect
   end
 
   def update
