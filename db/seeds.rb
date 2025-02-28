2cca36304b7f0ecaa44d156e2737e6f32e26f900
dev.goraebap <dev.goraebap@gmail.com>
2025-02-28 21:46:28 +0900

    feat: init default seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,64 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Role.create!([
+  { name: 'User' },
+  { name: 'Admin' }
+])
+
+User.create!(
+  email_address: Rails.application.credentials.dig(:admin, :email),
+  password: Rails.application.credentials.dig(:admin, :password),
+  password_confirmation: Rails.application.credentials.dig(:admin, :password),
+  role_id: 2,
+  nickname: '관리자',
+  bio: nil,
+  otp: nil,
+  otp_expiry_date: nil,
+  is_email_verified: true,
+  image_url: nil
+)
+
+Category.create!([
+  {
+    id: 1001,
+    name: '기술'
+  },
+  {
+    id: 1002,
+    name: '건강 & 웰니스'
+  },
+  {
+    id: 1003,
+    name: '여행'
+  },
+  {
+    id: 1004,
+    name: '금융'
+  },
+  {
+    id: 1005,
+    name: '음식 & 요리'
+  },
+  {
+    id: 1006,
+    name: '엔터테인먼트'
+  },
+  {
+    id: 1007,
+    name: '교육'
+  },
+  {
+    id: 1008,
+    name: '스포츠'
+  },
+  {
+    id: 1009,
+    name: '비즈니스'
+  },
+  {
+    id: 1010,
+    name: '라이프스타일'
+  }
+])
