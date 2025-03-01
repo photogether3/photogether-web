6a2408d2558bfe9c137a8c0a3525d4e4e73053e0
dev.goraebap <dev.goraebap@gmail.com>
2025-03-01 22:17:41 +0900

    feat: add post show usecase

--- app/controllers/api/v1/post_controller.rb
+++ app/controllers/api/v1/post_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PostController < Api::ApplicationApiController
   before_action :authenticate_user!
   before_action :get_collection_or_fail, only: [ :index, :create ]
+  before_action :get_post_or_fail, only: [ :show, :update, :destroy ]
 
   def index
     # 기본값 설정
@@ -21,12 +22,16 @@ class Api::V1::PostController < Api::ApplicationApiController
       total_item_count: posts.total_count,
       total_page_count: posts.total_pages,
       current_page: posts.current_page,
-      items: posts.map { |post| post.to_detail.merge(image_url: post.image.attached? ? url_for(post.image) : nil) }
+      items: posts.map { |post| post.to_detail.merge(
+        image_url: url_for(post.image)
+      ) }
     }, status: :ok
   end
 
   def show
-    puts "Post show"
+    render json: @post.to_detail.merge(
+      image_url: url_for(@post.image)
+    ), status: :ok
   end
 
   def create
@@ -61,5 +66,11 @@ class Api::V1::PostController < Api::ApplicationApiController
   def get_collection_or_fail
     @collection = Collection.find_by(id: params[:collectionId], user_id: @current_user.id)
     raise ActiveRecord::RecordNotFound, "사진첩을 찾을 수 없습니다." unless @collection
+  end
+
+  # 게시물을 조회하고 없으면 예외를 발생시킵니다.
+  def get_post_or_fail
+    @post = Post.find_by(id: params[:id], user_id: @current_user.id)
+    raise ActiveRecord::RecordNotFound, "게시물을 찾을 수 없습니다." unless @post
   end
 end
