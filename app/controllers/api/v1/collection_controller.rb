ea08df96b003f6240ffd61e45d71f8863c664b02
dev.goraebap <dev.goraebap@gmail.com>
2025-03-01 11:35:35 +0900

    feat: add update, destroy usecase

--- app/controllers/api/v1/collection_controller.rb
+++ app/controllers/api/v1/collection_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::CollectionController < Api::ApplicationApiController
+  before_action :authenticate_user!
+  before_action :get_category_or_fail, only: [ :create, :update ]
+  before_action :get_collection_or_fail, only: [ :show, :update, :destroy ]
+
   def index
     puts "Collection index"
   end
@@ -8,14 +12,46 @@ class Api::V1::CollectionController < Api::ApplicationApiController
   end
 
   def create
-    puts "Collection create"
+    Collection.create!(
+      title: params[:title],
+      category_id: @category.id,
+      user_id: @current_user.id,
+      type: "DEFAULT"
+    )
+
+    render json: { message: "사진첩이 생성되었어요." }, status: :ok
   end
 
   def update
-    puts "Collection update"
+    raise CustomError, "수정할 수 없는 사진첩입니다." if @collection.type != "DEFAULT"
+
+    @collection.update!(
+      title: params[:title],
+      category_id: @category.id
+    )
+
+    render json: { message: "사진첩이 업데이트되었어요." }, status: :ok
   end
 
   def destroy
-    puts "Collection destroy"
+    # 게시물을 가지고 있는 사진첩은 삭제 이전에
+    # 사용자의 휴지통 사진첩으로 게시물을 이동시키는 작업이 필요
+    @collection.destroy
+
+    render json: { message: "사진첩이 삭제되었어요." }, status: :ok
+  end
+
+  private
+
+  # 카테고리를 조회하고 없으면 예외를 발생시킵니다.
+  def get_category_or_fail
+    @category = Category.find_by(id: params[:categoryId])
+    raise ActiveRecord::RecordNotFound, "카테고리를 찾을 수 없습니다." unless @category
+  end
+
+  # 사진첩을 조회하고 없으면 예외를 발생시킵니다.
+  def get_collection_or_fail
+    @collection = Collection.find_by(id: params[:id], user_id: @current_user.id)
+    raise ActiveRecord::RecordNotFound, "사진첩을 찾을 수 없습니다." unless @collection
   end
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,13 @@
 class Collection < ApplicationRecord
+  # self.inheritance_column = nil은 ActiveRecord에서
+  # STI(Single Table Inheritance)를 비활성화하기 위해 사용됩니다.
+  # 기본적으로 ActiveRecord는 type이라는 컬럼을 사용하여 STI를 구현합니다.
+  # 즉, type 컬럼을 사용하여 상속된 클래스의 이름을 저장하고,
+  # 이를 통해 상속된 클래스의 인스턴스를 생성합니다.
+  # 하지만, type 컬럼을 다른 용도로 사용하고 싶거나,
+  # STI를 사용하지 않으려는 경우 self.inheritance_column을 nil로 설정하여
+  # ActiveRecord가 type 컬럼을 무시하도록 할 수 있습니다.
   self.inheritance_column = nil
+
+  validates :title, presence: true, length: { in: 2..50 }
 end
