c1020cf133616a1f3c6ece6ad21489abdf478b9e
dev.goraebap <dev.goraebap@gmail.com>
2025-03-01 10:09:15 +0900

    feat: add category index usecase

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::CategoryController < Api::ApplicationApiController
   def index
     puts "Category index"
+    categories = Category.order(:id)
+    render json: categories, status: :ok
   end
 
   def index_with_favorite
