45a6d6329750d7bb0e2c821478f53fa595aecdf1
dev.goraebap <dev.goraebap@gmail.com>
2025-03-03 19:52:47 +0900

    feat: add register page

--- app/javascript/controllers/email_check_controller.js
+++ app/javascript/controllers/email_check_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   check() {
+    console.log('hello')
     const email = this.element.value.trim();
     document.getElementById("email-check-result").textContent = "로딩 중..";
 
