4025ffb363b9d547bb24457c6344217a9751c98d
dev.goraebap <dev.goraebap@gmail.com>
2025-03-03 14:05:57 +0900

    chore: add favicon

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,5 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("app/assets/fonts")
 Rails.application.config.assets.precompile += %w[ *.woff2 *.woff *.ttf *.otf ]
+
+Rails.application.config.assets.precompile += %w[ favicon.ico ]
