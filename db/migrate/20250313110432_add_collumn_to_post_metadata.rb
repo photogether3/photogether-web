c416ccdc021f766f2b8d57599f85a21540cbadec
dev.goraebap <dev.goraebap@gmail.com>
2025-03-13 21:03:50 +0900

    refactor: 오타수정

--- db/migrate/20250313110432_add_collumn_to_post_metadata.rb
+++ db/migrate/20250313110432_add_collumn_to_post_metadata.rb
@@ -1,4 +1,4 @@
-class AddCollumnToPostMetadata < ActiveRecord::Migration[8.0]
+class AddColumnToPostMetadata < ActiveRecord::Migration[8.0]
   def change
     change_table :post_metadata do |t|
       t.boolean :has_link, null: false, default: false
