a7b961a3a342d820aa1dc3e30dbbc5d905f01679
dev.goraebap <dev.goraebap@gmail.com>
2025-03-29 11:54:45 +0900

    feat: 회원 관리 레이아웃 개선

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,7 @@
 class Admin::AdminController < ApplicationController
   allow_unauthenticated_access
-  http_basic_authenticate_with name: Rails.application.credentials.dig(:admin, :email),
-                               password: Rails.application.credentials.dig(:admin, :password)
+  http_basic_authenticate_with  name: Rails.application.credentials.dig(:admin, :email),
+                                password: Rails.application.credentials.dig(:admin, :password)
 
   layout "admin"
 end
---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,19 @@
+class Admin::CategoriesController < Admin::AdminController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
