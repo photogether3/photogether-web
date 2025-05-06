2c9d22710f7e0913613065915fdc52812a789697
dev-goraebap <dev.goraebap@gmail.com>
2025-05-06 14:20:59 +0900

    feat: CORS 설정에 추가 출처(origin) 추가

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # 여러 개를 지정할 수 있습니다.
     # 여기에 앱의 Origin (예: 'capacitor://localhost')과
     # 웹 개발 서버 Origin (예: 'http://localhost:4200') 등을 추가하세요.
-    origins "capacitor://localhost", "http://localhost:4200", "https://photogether.app" # <- 여기에 앱의 Origin과 웹 Origin 등을 , 로 구분하여 추가
+    origins "capacitor://localhost", "ionic://localhost", "http://localhost",
+        "http://localhost:4200", "http://10.0.2.2", "https://photogether.app"
 
     # resource 항목은 CORS 설정을 적용할 경로 패턴입니다.
     # '*'는 모든 경로를 의미합니다. '/api/*' 처럼 특정 경로만 지정할 수도 있습니다.
