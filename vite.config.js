eab1184195b044a2996ea27cc51df0ec68e23cf1
dev-goraebap <dev.goraebap@gmail.com>
2025-05-20 20:26:04 +0900

    chore: vite 기본 설정, tailwindcss 연결

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  build: {
+    outDir: "app/assets/builds",
+    rollupOptions: {
+      input: "./resources/main.js"
+    }
+  }
+})
