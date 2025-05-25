712afc2269a136e77f38e9af43a360e620bf1251
dev-goraebap <dev.goraebap@gmail.com>
2025-05-25 16:42:34 +0900

    feat: 사용자 프로필 페이지에 상세 정보 및 게시물 목록 추가

--- app/controllers/pages/users_controller.rb
+++ app/controllers/pages/users_controller.rb
@@ -6,6 +6,14 @@ class Pages::UsersController < Pages::SessionController
   ) }
 
   def show
-    render Pages::Users::Show.new(current_user: current_user)
+    user_result = current_user.to_detail
+    user_favorite_categories = current_user.favorite_categories
+    user_posts = current_user.posts.order(created_at: :desc).map(&:to_detail)
+
+    render Pages::Users::Show.new(
+      user: user_result,
+      favorite_categories: user_favorite_categories,
+      posts: user_posts
+    )
   end
 end
