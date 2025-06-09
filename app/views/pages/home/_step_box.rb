c12f24e5f742feb6adeab72940a3b1ec3eec0c7d
dev-goraebap <dev.goraebap@gmail.com>
2025-06-09 14:45:57 +0900

    wip: 스탭박스 구현

---
+++ app/views/pages/home/_step_box.rb
@@ -0,0 +1,33 @@
+class Views::Pages::Home::StepBox < Views::Base
+  def initialize(
+    title: "스크린샷을 찍어주세요",
+    step: "01",
+    content: "지금 화면을 캡처하거나, 이미 앨범에 저장된 스크린샷을 불러와도 괜찮아요.",
+    active: false,
+    dummy: false
+  )
+    @title = title
+    @step = step
+    @content = content
+    @dummy = dummy
+    @border = active ? "border-primary" : "border-[#A6A6A6]"
+  end
+
+  def view_template
+    unless @dummy
+      div(class: "flex flex-col gap-1 border #{@border} bg-base-300 rounded-xl
+          p-2 w-[150px] lg:w-[323px] lg:p-[24px] lg:gap-3") do
+        div(class: "flex justify-between items-center") do
+          p(class: "text-[10px] lg:text-xl font-bold") { @title }
+          div(class: "flex items-center justify-center rounded-full bg-secondary text-secondary-content border border-primary
+            text-[9px] w-[20px] h-[20px] lg:text-base lg:min-w-[36px] lg:min-h-[36px]") do
+            @step
+          end
+        end
+        p(class: "text-[10px] lg:text-xs text-[#C6C6C6]") { @content }
+      end
+    else
+      div(class: "w-[150px] lg:w-[323px]")
+    end
+  end
+end
--- app/views/pages/home/index.rb
+++ app/views/pages/home/index.rb
@@ -165,19 +165,12 @@ class Views::Pages::Home::Index < Views::Base
                   img(src: "/images/landing/section02/item07.png", class: "object-contain")
                 end
                 div(class: "flex flex-col gap-[13px]") do
-                  # 이벤트박스
-                  div(class: "flex flex-col gap-1 border border-primary bg-base-300 rounded-xl
-                      p-2 w-[150px]") do
-                    div(class: "flex justify-between items-center") do
-                      p(class: "text-[10px] font-bold") { "스크린샷을 찍어주세요." }
-
-                      div(class: "flex items-center justify-center rounded-full bg-secondary text-secondary-content border border-primary
-                        text-[9px] w-[20px] h-[20px]") do
-                        "01"
-                      end
-                    end
-                    p(class: "text-[10px] text-[#C6C6C6]") { "지금 화면을 캡처하거나, 이미 앨범에 저장된 스크린샷을 불러와도 괜찮아요." }
-                  end
+                  # 스탭박스
+                  render Views::Pages::Home::StepBox.new(
+                    title: "스크린샷을 찍어주세요.",
+                    step: "01",
+                    content: "지금 화면을 캡처하거나, 이미 앨범에 저장된 스크린샷을 불러와도 괜찮아요."
+                  )
 
                   # 컨트롤러 박스
                   div(class: "flex gap-[13px]") do
@@ -209,14 +202,34 @@ class Views::Pages::Home::Index < Views::Base
                 end
               end
               # 이미지 영역 (데스크탑용)
-              div(class: "hidden sm:flex w-[1104px] h-[727px] gap-[80px] items-end") do
-                div(class: "w-[671px] h-[464px] border") do
-                  div(class: "text-3xl line-through") do
-                    "상당히 귀찮은 영역(귀찮은 일 미루깃)"
-                  end
+              div(class: "hidden sm:flex w-[1104px] gap-[80px] items-end") do
+                div(class: "w-[700px] h-[464px] flex flex-wrap gap-[25px]") do
+                  render Views::Pages::Home::StepBox.new(dummy: true)
+                  render Views::Pages::Home::StepBox.new(
+                    title: "스크린샷을 찍어주세요.",
+                    step: "01",
+                    content: "지금 화면을 캡처하거나, 이미 앨범에 저장된 스크린샷을 불러와도 괜찮아요.",
+                    active: true
+                  )
+                  render Views::Pages::Home::StepBox.new(
+                    title: "업로드를 눌러주세요.",
+                    step: "02",
+                    content: "불러오고 싶은 스크린샷을 선택한 후, ‘업로드’ 버튼을 눌러 포토게더에 등록해보세요"
+                  )
+                  render Views::Pages::Home::StepBox.new(dummy: true)
+                  render Views::Pages::Home::StepBox.new(
+                    title: "텍스트를 추출 합니다.",
+                    step: "03",
+                    content: "스크린샷 안에 담긴 문구와 정보를 자동으로 분석해, 텍스트 형태로 깔끔하게 정리해드립니다."
+                  )
+                  render Views::Pages::Home::StepBox.new(
+                    title: "폴더로 정리하세요.",
+                    step: "04",
+                    content: "정리 기준은 사용자 마음대로! 폴더를 직접 선택해 필요한 대로 분류할 수 있어요."
+                  )
                 end
 
-                div(class: "h-full relative") do
+                div(class: "h-full") do
                   img(src: "/images/landing/section02/item07.png", class: "object-contain")
                 end
               end
