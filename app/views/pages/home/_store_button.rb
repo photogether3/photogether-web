f88e2d9f589c365261c3cb2742b49ff241834800
dev-goraebap <dev.goraebap@gmail.com>
2025-06-11 14:57:17 +0900

    feat: 모바일, 데스크탑에 따라 QR코드 또는 앱 링크 제공

--- app/views/pages/home/_store_button.rb
+++ app/views/pages/home/_store_button.rb
@@ -3,11 +3,22 @@ class Views::Pages::Home::StoreButton < Views::Base
     @platform = platform
     @image_src = platform == "ios"? "/images/landing/section01/appstore.png"
                                   : "/images/landing/section01/googleplay.png"
+    @mobile_link = platform == "ios" ? "https://apps.apple.com/kr/app/%ED%8F%AC%ED%86%A0%EA%B2%8C%EB%8D%94/id6745507306"
+                                    : "https://play.google.com/apps/test/com.photogether.app/2025051067"
   end
 
   def view_template
-    a(href: "/qr-code?platform=#{@platform}", data: { turbo_method: "get", turbo_frame: "modal_overlay" }) do
-      img(src: @image_src, class: "w-[97px] sm:w-[120px] lg:w-[180px] object-contain")
+    # 모바일용: 스토어 링크로 바로 연결
+    div(class: "lg:hidden") do
+      a(href: @mobile_link) do
+        img(src: @image_src, class: "w-[97px] sm:w-[120px] object-contain")
+      end
+    end
+    # 데스크탑용: QR 코드 이미지 노출
+    div(class: "hidden lg:block") do
+      a(href: "/qr-code?platform=#{@platform}", data: { turbo_method: "get", turbo_frame: "modal_overlay" }) do
+        img(src: @image_src, class: "w-[180px] object-contain")
+      end
     end
   end
 end
